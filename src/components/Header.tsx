f8a9d6ab4a5fd3fb17d8178038e6746aa0fed391
nicksales7 <nicksales72@gmail.com>
2023-04-03 15:45:42 -0400

    Fix: Fixed for non-desktop devices

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,12 +6,13 @@ export default function Header() {
         <>
             <div className="topNavigation">
                 <h3>NICHOLAS SALES</h3>
-                <img className="selfPortrait" src="/self-portrait.png" alt="Self-Portrait"></img>
+                <a href="./index.js">
+                <img className="selfPortrait" src="/self-portrait.png" alt="Self-Portrait" />
+                </a>
                 <a href="contact.js">Contact</a>
                 <a href="work.js">Work</a>
                 <a href="projects.js">Projects</a>
                 <a href="about.js">About</a>
-                <a href="index.js">Home</a>
             </div>
         </>
 
