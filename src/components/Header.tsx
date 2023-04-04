f9e052bbb8c3393021af3867f446ac25bc5a2ff9
nicksales7 <nicksales72@gmail.com>
2023-04-03 23:18:27 -0400

    Fix: Mobile devices formatting

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,5 @@
 import "./Header.css";
+import selfPortrait from "../assets/self-portrait.png"
 
 export default function Header() {
 
@@ -6,14 +7,14 @@ export default function Header() {
         <>
             <div className="topNavigation">
                 <h3>NICHOLAS SALES</h3>
-                <a href="./index.js">
-                <img className="selfPortrait" src="/self-portrait.png" alt="Self-Portrait" />
-                </a>
                 <a href="contact.js">Contact</a>
                 <a href="work.js">Work</a>
                 <a href="projects.js">Projects</a>
                 <a href="about.js">About</a>
             </div>
+            <a href="./index.js">
+            <img className="selfPortrait" src={selfPortrait} alt="Self-Portrait"/>
+            </a>
         </>
 
     )
