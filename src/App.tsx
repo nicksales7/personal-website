15199071f96f95efeb6cb65c59a66d25821c8ea4
nicksales7 <nicksales72@gmail.com>
2023-04-06 13:23:01 -0400

    feat: ported basic website to MUI

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,71 @@
-import "./App.css"
+import { Box, Typography, useTheme } from "@mui/material";
 import Header from "./components/Header";
 
 function App() {
-    return (
-        <div className="App">
-            <Header></Header>
-            <h1>HEY THERE! I'M NICHOLAS SALES</h1>
-            <h2>I am a 19-year-old full-stack software engineer currently studying computer science at Carleton University.
-    I am passionate about creating clean and efficient code, and I love to work on a variety of projects, spanning all the way from games and websites,
-    to automating daily tasks. With a strong belief in a fully autonomous future, I am always on the lookout for new and exciting
-    internship opportunities. On this website, you will find information about my skills, projects, and experience.</h2>
-        </div>
-);
+  const theme = useTheme();
+
+  return (
+    <Box
+      sx={{
+        position: "fixed",
+        top: 0,
+        right: 0,
+        bottom: 0,
+        left: 0,
+        backgroundImage: 'url("https://i.imgur.com/mtgsbMS.png")',
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+      }}
+    >
+      <Header />
+      <Box
+        sx={{
+          position: "fixed",
+          left: 0,
+          right: 0,
+          top: "40%",
+          transform: "translateY(-50%)",
+          textAlign: "center",
+          zIndex: 2,
+        }}
+      >
+        <Typography
+          variant={"h1"}
+          sx={{
+            fontFamily: "Montserrat, sans-serif",
+            color: "#373737",
+            fontSize: {
+              xs: theme.typography.pxToRem(32),
+              sm: theme.typography.pxToRem(38),
+              md: theme.typography.pxToRem(44),
+              lg: theme.typography.pxToRem(50),
+              xl: theme.typography.pxToRem(56),
+            },
+          }}
+        >
+          HEY THERE, I'M NICHOLAS SALES
+        </Typography>
+        <Typography
+          variant={"h1"}
+          sx={{
+            fontFamily: "Montserrat, sans-serif",
+            color: "#373737",
+            fontSize: {
+              xs: theme.typography.pxToRem(10),
+              sm: theme.typography.pxToRem(11),
+              md: theme.typography.pxToRem(13),
+              lg: theme.typography.pxToRem(15),
+              xl: theme.typography.pxToRem(20),
+            },
+          }}
+        >
+          I am a 19-year-old full-stack software engineer currently studying computer science at Carleton University. I am passionate about creating clean and efficient code, 
+          and I love to work on a variety of projects, spanning all the way from games and websites, to automating daily tasks. With a strong belief in a fully autonomous future, 
+          I am always on the lookout for new and exciting internship opportunities. On this website, you will find information about my skills, projects, and experience.
+        </Typography>
+      </Box>
+    </Box>
+  );
 }
 
 export default App;
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,21 +1,63 @@
-import "./Header.css";
 import selfPortrait from "../assets/self-portrait.png"
+import { Avatar, Box, Button, Typography, useTheme, } from "@mui/material";
 
 export default function Header() {
+  const theme = useTheme();
 
-    return (
-        <>
-            <div className="topNavigation">
-                <h3>NICHOLAS SALES</h3>
-                <a href="contact.js">Contact</a>
-                <a href="work.js">Work</a>
-                <a href="projects.js">Projects</a>
-                <a href="about.js">About</a>
-            </div>
-            <a href="./index.js">
-            <img className="selfPortrait" src={selfPortrait} alt="Self-Portrait"/>
-            </a>
-        </>
-
-    )
+  return (
+    <div className="headerSection">
+      <a href="https://nicksales.dev/">
+        <Avatar
+          alt="Nicholas Sales"
+          src={selfPortrait}
+          sx={{
+            position: "fixed",
+            height: "65px",
+            width: "65px",
+            top: 15,
+            left: 20,
+            zIndex: 3,
+          }}
+        />
+      </a>
+      <Typography
+        variant={"h1"}
+        sx={{
+          position: "fixed",
+          fontFamily: "Montserrat, sans-serif",
+          color: "#373737",
+          top: 35,
+          left: 100,
+          zIndex: 2,
+          fontSize: "20px",
+        }}
+      >
+        NICHOLAS SALES
+      </Typography>
+      <Button
+        variant="text"
+        sx={{
+          position: "fixed",
+          bottom: theme.spacing(15),
+          right: theme.spacing(15),
+          zIndex: 2,
+          fontSize: "15px",
+          fontFamily: "Montserrat, sans-serif",
+          color: "#373737",
+        }}
+      />
+      <Box
+        sx={{
+          position: "fixed",
+          top: 1,
+          left: 0,
+          width: 1,
+          height: 100,
+          backgroundColor: "#e7e7e7",
+          borderRadius: "40px 40px 40px 40px",
+          zIndex: 1,
+        }}
+      />
+    </div>
+  );
 }
